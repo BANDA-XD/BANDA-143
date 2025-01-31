582bf892b7f97b6ae6dc577c4dd4c407a9b5adb0
BANDA-XD <mohamed.banda.xd@gmail.com>
2025-01-31 22:56:28 +0100

    Update BANDA.py

--- BANDA.py
+++ BANDA.py
@@ -1,4 +1,9 @@
-import os
+#!/data/data/com.termux/files/usr/bin/python
+import platform,os
+#####
 os.system("git pull")
-os.system('xdg-open https://chat.whatsapp.com/ImXknPvyvU9IFBballhWgy')
-import old
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import old
+elif bit == '32bit':
+    import old
