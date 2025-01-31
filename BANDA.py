b82cfe2168368dc8ee7f2e36c2369d7e22bcdd37
BANDA-XD <mohamed.banda.xd@gmail.com>
2025-01-31 23:11:02 +0100

    Update BANDA.py

--- BANDA.py
+++ BANDA.py
@@ -1,3 +1,7 @@
-import os
-os.system("git pull")
-import p64
+import os,time,platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import OMD32
+elif bit=='32bit':
+    import OLD64
